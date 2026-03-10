8fb6b6dd9fd743b7830d63bf9e3ce3833020456f
Matthias Theißen <matthias.theissen@scoop-software.de>
2026-03-10 14:50:37 +0100

    Bump to v1.0.1 - Set exact Dart SDK and add workflow for F-Droid

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -53,8 +53,8 @@ defaultConfig {
 
     minSdk = 23
     targetSdk = 35  // Android 14
-    versionCode = 1
-    versionName = "1.0.0"
+    versionCode = 2
+    versionName = "1.0.1"
 }    
 
     buildTypes {
