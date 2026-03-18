239f47696613804496429e88827ef507e39f5b7e
Matthias Theißen <matthias.theissen@scoop-software.de>
2026-03-18 21:29:47 +0100

    Fix: Use correct notification icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -53,8 +53,8 @@ defaultConfig {
 
     minSdk = flutter.minSdkVersion
     targetSdk = 35  // Android 14
-    versionCode = 5
-    versionName = "1.1.1"
+    versionCode = 6
+    versionName = "1.1.2"
 }    
 
     buildTypes {
